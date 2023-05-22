a242e03e5b45d5aeaf556032d4de97540bd177c3
hugo.andriamaromanana@laplateforme.io <hugo.andriamaromanana@laplateforme.io>
2023-05-22 17:01:24 +0200

    [FIX] Now using JS!

--- main.py
+++ main.py
@@ -1,45 +1,21 @@
 from flask import Flask, render_template, request
 from api import *
 from data import languages_data, languages_data_swapped
-app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
-
+app = Flask(__name__, template_folder='templateFiles',
+            static_folder='staticFiles')
 
 
 @app.route('/', methods=['GET', 'POST'])
-
-def dropdown_from():
-
-    output_text = ''
-
+def dropdown_form():
     if request.method == 'POST':
-        
-        selected_language = str(request.get_data('language'))[
-            :str(request.get_data('language')).find('input_text')]
-
-        src = languages_data_swapped[selected_language[:selected_language.find(
-            '&')].replace('language1=', '')[2:]].replace('+', ' ').replace('%27', "'").replace('%2C', ',')
-        dest = languages_data_swapped[selected_language[selected_language.find(
-            '&'):].replace('language2=', '')[1:-1]].replace('+', ' ').replace('%27', "'").replace('%2C', ',')
-        print(src, dest)
-
-        input_text = str(request.get_data('language'))[str(request.get_data(
-            'language')).find('input_text') + 10:][1:-1].replace('+', ' ')
-        print(input_text)
-
-        output_text = translate(input_text, dest, src)
-        print(output_text)
-
-        if output_text == input_text:
-            output_text = 'Sorry, I cannot translate this text. Please try again.'
-
-        if src == dest:
-            output_text = input_text
-
-        if output_text == '':
-            output_text = 'Empty fill. Please try again.'
+        selected_language1 = request.form['language1']
+        selected_language2 = request.form['language2']
+        input_text = request.form['input_text']
+        print(selected_language1, selected_language2, input_text)
+        return translate(input_text, languages_data_swapped[selected_language2], languages_data_swapped[selected_language1])
 
     languages = [languages_data[i] for i in languages_data.keys()]
-    return render_template('index.html', languages=languages,output_text=output_text)
+    return render_template('index.html', languages=languages, selected_language1="", selected_language2="")
 
 
 if __name__ == "__main__":
